be4c19ea031945c15f1250450c796c9bf62a7bbf
Prachi1808 <prachimohanty.1808@gmail.com>
2022-03-16 20:16:23 +0530

    Enter valid First Name

---
+++ src/JUnitPackage/JUnitExample.java
@@ -0,0 +1,14 @@
+package JUnitPackage;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class JUnitExample {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
---
+++ src/JUnitPackage/Name.java
@@ -0,0 +1,15 @@
+package JUnitPackage;
+
+
+	
+	import java.util.regex.Pattern;
+
+	public class Name 
+	{
+		    private static final String FIRST_NAME_PATTERN = "^[A-Z]{1}[a-z]{2,}$";
+
+		    public boolean validateFirstName(String firstName) {
+		        Pattern pattern = Pattern.compile(FIRST_NAME_PATTERN);
+		        return pattern.matcher(firstName).matches();
+		    }
+		}
