3a5a29bbeb1fa5832e0ab3549da84d50baac5e2a
Prachi1808 <prachimohanty.1808@gmail.com>
2022-03-16 20:30:00 +0530

    Check the Mobile number Validation

--- src/JUnitPackage/JUnitExample.java
+++ src/JUnitPackage/JUnitExample.java
@@ -1,14 +1,69 @@
 package JUnitPackage;
 
 import static org.junit.jupiter.api.Assertions.*;
-
+import org.junit.Assert;
 import org.junit.jupiter.api.Test;
 
-class JUnitExample {
+class JUnitExample<UserValidation> {
 
-	@Test
-	void test() {
-		fail("Not yet implemented");
-	}
-
-}
+		    @Test
+		    public void givenFirstName_WhenProper_ShouldReturn_True() {
+		        UserValidation userValidation = new UserValidation();
+		        boolean result = userValidation.validateFirstName("Prachi");
+		        Assert.assertEquals(true,result);
+		    }
+		    @Test
+		    public void givenFirstName_WhenNotProper_ShouldReturn_False() {
+		        UserValidation userValidation = new UserValidation();
+		        boolean result = userValidation.validateFirstName("Pr");
+		        Assert.assertEquals(false,result);
+		    }
+		    @Test
+		    public void givenLastName_WhenProper_ShouldReturn_True() {
+		        UserValidation userValidation = new UserValidation();
+		        boolean result = userValidation.validateLastName("Mohanty");
+		        Assert.assertEquals(true,result);
+		    }
+		    @Test
+		    public void givenLastName_WhenNotProper_ShouldReturn_False() {
+		    	UserValidation  userValidation = new UserValidation ();
+		        boolean result = userValidation.validateLastName("Mo");
+		        Assert.assertEquals(false,result);
+		    }
+		    @Test
+		    public void givenEmail_WhenProper_ShouldReturn_True() {
+		        UserValidation userValidation = new UserValidation ();
+		        boolean result = userValidation.validateEmail("abc.xyz@bl.co.in");
+		        Assert.assertEquals(true,result);
+		    }
+		    @Test
+		    public void givenEmail_WhenNotProper_ShouldReturn_False() {
+		    	UserValidation userValidation = new UserValidation();
+		        boolean result = userValidation.validateEmail("prachi@.com");
+		        Assert.assertEquals(false,result);
+		    }
+		    @Test
+		    public void givenPhone_WhenProper_ShouldReturn_True() {
+		        UserValidation userValidation = new UserValidation();
+		        boolean result = userValidation.validatePhone("918105215414");
+		        Assert.assertEquals(true,result);
+		    }
+		    @Test
+		    public void givenPhone_WhenNotProper_ShouldReturn_False() {
+		        UserValidation userValidation = new UserValidation();
+		        boolean result = userValidation.validatePhone("8105215414");
+		        Assert.assertEquals(false,result);
+		    }
+		    @Test
+		    public void givenPassword_WhenProper_ShouldReturn_True() {
+		        UserValidation userValidation = new UserValidation();
+		        boolean result = userValidation.validatePassword("abcdfdA@fg0");
+		        Assert.assertEquals(true,result);
+		    }
+		    @Test
+		    public void givenPassword_WhenNotProper_ShouldReturn_False() {
+		    	UserValidation  userValidation = new UserValidation();
+		        boolean result = userValidation.validatePassword("adasdas0daS");
+		        Assert.assertEquals(false,result);
+		    }
+		}
